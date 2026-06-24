04187514fd87263d2b504a2ca7011d71e6d0269b
Clau3333 <cla.cordovam@duocuc.cl>
2026-06-24 14:47:35 -0400

    test(coverage): agrega pruebas unitarias y evidencias de cobertura

--- src/test/java/cl/perfulandia/usuarios/security/PasswordUtilTest.java
+++ src/test/java/cl/perfulandia/usuarios/security/PasswordUtilTest.java
@@ -1,8 +1,12 @@
 package cl.perfulandia.usuarios.security;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordUtilTest {
 
